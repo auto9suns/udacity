e9657a1bbe9ee2f14a987727175cdc6171e9c36e
Xu <xuche.study@gmail.com>
2017-07-05 20:41:28 -0700

    titanic project 1. original file: data and visuals.py 2. jupyter notebook file 3. html version of notebook: report.html 4. dmapi file for api used to manipulate data in future, store in this repo tmp dmApi_xc.py

---
+++ machine-learning/titanic_survival_exploration/dmApi_xc.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import numpy as np
+
+
+class dfManipulator():
+  def __init_():
+    pass
+
+  def add_column(df):
+    if isinstance(df,pd.DataFrame):
+        pass
+    else:
+        print "ERROR: input is not pd.DataFrame type"
